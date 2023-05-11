8eb14a4f3784336f6d9d936b0dc87ecf8b0c4cc1
Elide <zavalavinagreelide@gmail.com>
2023-05-11 09:33:14 -0600

    useState

--- src/couterApp.jsx
+++ src/couterApp.jsx
@@ -1,16 +1,38 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import { useState } from "react";
 
+const handle = (e) => {
+  console.log(e);
+};
 const CouterApp = ({ value }) => {
-    return (
-        <>
-            <h1>CounterApp { value } </h1>
+  console.log("render");
 
-        </>
-    )
-}
+  let [counter, setCounter] = useState(value);
+  const style = () => {
+    if (counter > 10) {
+      return {
+        color: "red",
+      };
+    } else {
+      return {
+        color: "green",
+      };
+    }
+  };
 
-export default CouterApp
+  return (
+    <>
+      <h1 style={style()}>CounterApp {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}> +1</button>
+      <button onClick={() => setCounter(value)}> Reset</button>
+      <button onClick={() => setCounter(counter - 1)}> -1</button>
+      <button onClick={handle}>Evento</button>
+    </>
+  );
+};
 
-CouterApp.propTypes = { 
-    value: PropTypes.number.isRequired
-}  
+export default CouterApp;
+
+CouterApp.propTypes = {
+  value: PropTypes.number.isRequired,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import { Otro } from './otro';
 // import App from './HelloWorldApp';
-import './styles.css';
-import CouterApp from './couterApp';
-
+import "./styles.css";
+import CouterApp from "./couterApp";
 
 // const title = 'React web of Elide';
 const value = 10;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        {/* <App /> */}
-        {/* <Otro/> */}
-        <CouterApp value={ value } />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <Otro/> */}
+    <CouterApp value={value} />
+  </React.StrictMode>
 );
